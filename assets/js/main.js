4e5b78aaa25f3baccae400f8ebf4674f206e2aa6
Nam Dao <nadao@davidson.edu>
2023-10-17 00:25:16 -0400

    first draft

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
+function totalScore(){
+    let totalScore = 0;
 
-/* javascript */
+    Array.from(document.querySelectorAll('.score'))
+    .forEach(input => totalScore += parseFloat(input.value));
+
+    totalScore = totalScore * 2.5;
+
+    console.log(totalScore);
+
+    // Display the total score in the result element
+    const total = document.getElementById("total");
+    total.value = totalScore;
+}
